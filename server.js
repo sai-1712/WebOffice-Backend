04d8a4f066bb4ec365cb672a1dbf7fe1a39c1a46
Sai Kiran <ravirala.sai1712@gmail.com>
2023-04-28 10:02:07 +0530

    testt

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ const app = require("./app");
 const { dbconnection } = require("./config/database");
 dbconnection();
 app.listen(8000, () => {
+    console.log('hello');
     console.log("Website running on port:8000");
 });
